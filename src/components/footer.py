6ff1fb1693651f62c7d1732519b5b1b0b4bc3e14
Sathwik Reddy <sathwik661119@gmail.com>
2026-09-22 02:35:02 +0530

    Add home screen and UI layout

---
+++ src/components/footer.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def footer_home():
+
+    st.markdown("""
+    <div style="margin-top:2rem;display:flex;gap:6px;justify-content:center;align-items:center">
+    <p>Developed By <span style="color:black;font-weight:bold;"> Baddipadaga Sathwik Reddy</span> </p>
+    </div>
+    
+    """,unsafe_allow_html=True)
